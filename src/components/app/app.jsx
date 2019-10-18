05c697a7fb3cd842e08c6ebe2aa93fe77baedeec
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-10-18 17:03:43 +0300

    Render 5 articles without arguments

---
+++ src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MainScreen from "../main-screen/main-screen.jsx";
+
+const App = () => {
+  return <MainScreen
+  />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app.jsx";
+import App from "./components/app/app.jsx";
 
 const init = () => {
   ReactDOM.render(
-      <App />,
+      <App
+      />,
       document.querySelector(`#root`)
   );
 };
