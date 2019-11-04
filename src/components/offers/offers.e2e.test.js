9b395da6418e8a935d9f6f06d3dd64d4b4d2844d
Maksim Akimov <maksimakimov@mBookPro15.local>
2019-11-04 15:54:31 +0300

    e2e-test uses simple mocks

--- src/components/offers/offers.e2e.test.js
+++ src/components/offers/offers.e2e.test.js
@@ -2,7 +2,55 @@ import React from "react";
 import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Offers from "./offers.jsx";
-import {offers} from "../../mocks/offers.js";
+
+const offers = [
+  {
+    id: 111,
+    city: {
+      location: {
+        name: `City Name`,
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 12
+      }
+    },
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+      zoom: 12
+    },
+    description: `Some text`,
+    type: `Apartment`,
+    price: 120,
+    image: `img/apartment-01.jpg`,
+    rating: 100,
+    isPremium: true,
+    isBookmark: false
+  },
+  {
+    id: 222,
+    city: {
+      location: {
+        name: `City Name`,
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 12
+      }
+    },
+    location: {
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
+      zoom: 12
+    },
+    description: `Some text`,
+    type: `Private room`,
+    price: 80,
+    image: `img/room.jpg`,
+    rating: 50,
+    isPremium: false,
+    isBookmark: true,
+  }
+];
 
 const mocks = offers;
 
@@ -11,6 +59,7 @@ Enzyme.configure({adapter: new Adapter()});
 describe(`Offers mount rendering`, () => {
   const wrapper = mount(<Offers offers={mocks} />);
   const offerOne = wrapper.find(`.place-card`).first();
+  const offerTwo = wrapper.find(`.place-card`).last();
 
   it(`Offers have initial state`, () => {
     expect(wrapper.state().active).toEqual(null);
@@ -18,7 +67,7 @@ describe(`Offers mount rendering`, () => {
 
   it(`Offers change state on mouse hover first offer`, () => {
     offerOne.simulate(`mouseover`);
-    expect(wrapper.state().active).toEqual(1);
+    expect(wrapper.state().active).toEqual(111);
   });
 
   it(`Offers change state to initial on mouse leave first offer`, () => {
@@ -26,4 +75,8 @@ describe(`Offers mount rendering`, () => {
     expect(wrapper.state().active).toEqual(null);
   });
 
+  it(`Offers change state on mouse hover second offer`, () => {
+    offerTwo.simulate(`mouseover`);
+    expect(wrapper.state().active).toEqual(222);
+  });
 });
